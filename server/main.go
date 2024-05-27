ee70f61a0af6eeea79d5029af0bdfa2b20672950
Aqua Nelida <aquaneli@et-a6.kzn.21-school.ru>
2024-05-27 19:19:19 +0300

    serve

--- server/main.go
+++ server/main.go
@@ -4,13 +4,21 @@ import (
     "net/http"
 )
 
-type msg string
-func (m msg) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
-   fmt.Fprint(resp, m) 
-}
+
 
 func main() {
-    msgHandler := msg("Hello from Web Server in Go")
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+        http.ServeFile(w, r, "home.html")
+    })
+
+    http.HandleFunc("/postform", func(w http.ResponseWriter, r *http.Request){
+     
+        name := r.FormValue("username")
+        age := r.FormValue("userage")
+         
+        fmt.Fprintf(w, "Имя: %s Возраст: %s", name, age)
+    })
+
     fmt.Println("Server is listening...")
-    http.ListenAndServe("localhost:8181", msgHandler)
+    http.ListenAndServe("localhost:8181", nil)
 }
